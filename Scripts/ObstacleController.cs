826ae1b837b1996465ea80d2c37a921923911e52
unknown <valenciano3192006@gmail.com>
2026-09-23 12:24:36 +0200

    23/09/2026

--- Scripts/ObstacleController.cs
+++ Scripts/ObstacleController.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
+    private float minSize = 0.3f;
+    private float maxSize = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float size = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(size, size, 1);
     }
 
     // Update is called once per frame
