915e81918fdbb863967554aa71430e5c8f59d1df
unknown <valenciano3192006@gmail.com>
2026-09-24 14:34:24 +0200

    24/09/2026

--- Scripts/ObstacleController.cs
+++ Scripts/ObstacleController.cs
@@ -9,6 +9,10 @@ public class ObstacleController : MonoBehaviour
     {
         float size = Random.Range(minSize, maxSize);
         transform.localScale = new Vector3(size, size, 1);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Transform tf = GetComponent<Transform>();
+        rb.AddForce(Vector2.up * 34);
+        Debug.Log(transform == tf);
     }
 
     // Update is called once per frame
