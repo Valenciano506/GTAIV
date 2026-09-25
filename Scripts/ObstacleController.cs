a478147d13a9b2c79b9d9035885b486ab1f70689
unknown <valenciano3192006@gmail.com>
2026-09-25 13:05:55 +0200

    25/09/2026

--- Scripts/ObstacleController.cs
+++ Scripts/ObstacleController.cs
@@ -2,16 +2,35 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
-    private float minSize = 0.3f;
-    private float maxSize = 2f;
+    [SerializeField]
+    private float minSize = 0.01f;
+    [SerializeField]
+    private float maxSize = 0.05f;
+    [SerializeField]
+    private float minForce = 20f;
+    [SerializeField]
+    private float maxForce = 48f;
+    [SerializeField]
+    private float minTorque = 20f;
+    [SerializeField]
+    private float maxTorque = 48f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Asing a random size to each obstacle object
         float size = Random.Range(minSize, maxSize);
         transform.localScale = new Vector3(size, size, 1);
+        // Get the rigidbody component on our game object
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         Transform tf = GetComponent<Transform>();
-        rb.AddForce(Vector2.up * 34);
+        Vector2 randomDirection = Random.insideUnitCircle;
+        // Generate a random force
+        float force = Random.Range(minForce, maxForce);
+        rb.AddForce(randomDirection * force);
+        // Apply a random torque for each obstacle
+        float torque = Random.Range(minTorque, maxTorque);
+        rb.AddTorque(-torque);
+
         Debug.Log(transform == tf);
     }
 
